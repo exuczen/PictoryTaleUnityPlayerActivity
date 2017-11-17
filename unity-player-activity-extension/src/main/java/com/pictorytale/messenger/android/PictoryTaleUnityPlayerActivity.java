eb0d6b1336fe5850500ad60ea12aed05e7123fc0
Marcin Świtała <marcin.switala.wwa@gmail.com>
2017-11-17 21:54:32 +0100

    Google authorisation.

--- unity-player-activity-extension/src/main/java/com/pictorytale/messenger/android/PictoryTaleUnityPlayerActivity.java
+++ unity-player-activity-extension/src/main/java/com/pictorytale/messenger/android/PictoryTaleUnityPlayerActivity.java
@@ -6,15 +6,33 @@ import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.MessagingUnityPlayerActivity;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.unity3d.player.UnityPlayer;
 
-public class PictoryTaleUnityPlayerActivity extends MessagingUnityPlayerActivity
+public class PictoryTaleUnityPlayerActivity extends MessagingUnityPlayerActivity implements
+		GoogleApiClient.OnConnectionFailedListener
 {
 	private static final String TAG = PictoryTaleUnityPlayerActivity.class.getSimpleName();
 
@@ -25,6 +43,12 @@ public class PictoryTaleUnityPlayerActivity extends MessagingUnityPlayerActivity
 
 	public static final int SHARE_FILE_REQUEST_CODE = 123;
 	public static final int GOOGLE_SIGN_IN_REQUEST_CODE = 124;
+
+	// [START declare_auth]
+//	private FirebaseAuth mAuth;
+	// [END declare_auth]
+
+	private GoogleApiClient mGoogleApiClient;
 
 	public static PictoryTaleUnityPlayerActivity instance;
 
@@ -64,6 +88,7 @@ public class PictoryTaleUnityPlayerActivity extends MessagingUnityPlayerActivity
 		setSystemUiVisibility(true, true);
 		addUiVisibilityChangeListener();
 		PictoryTaleUnityPlayerActivity.instance = this;
+		com.google.android.gms.auth.api.signin.internal.SignInConfiguration asd;
 	}
 	@Override
 	protected void onStart()
@@ -81,9 +106,74 @@ public class PictoryTaleUnityPlayerActivity extends MessagingUnityPlayerActivity
 		}
 		else if (requestCode == PictoryTaleUnityPlayerActivity.GOOGLE_SIGN_IN_REQUEST_CODE)
 		{
-
+			GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+			if (result.isSuccess()) {
+				// Google Sign In was successful, authenticate with Firebase
+				GoogleSignInAccount account = result.getSignInAccount();
+//				firebaseAuthWithGoogle(account);
+				sendMessageToUnityObject(googleTokenReceiverUnityGameObjectName, googleTokenReceivedUnitySuccessCallbackName, account.getIdToken());
+			} else {
+				// Google Sign In failed, update UI appropriately
+				// [START_EXCLUDE]
+				// [END_EXCLUDE]
+				sendMessageToUnityObject(googleTokenReceiverUnityGameObjectName, googleTokenReceivedUnitySuccessCallbackName, "");
+			}
 		}
 	}
+
+	public void signInToGoogleAccount(String defaultWebClientId)
+	{
+		Log.e(TAG, "signInToGoogleAccount: defaultWebClientId=" +defaultWebClientId);
+		// [START config_signin]
+		// Configure Google Sign In
+		GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+				.requestIdToken(defaultWebClientId)
+				.requestEmail()
+				.build();
+		// [END config_signin]
+
+		mGoogleApiClient = new GoogleApiClient.Builder(this)
+				//.enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+				.addOnConnectionFailedListener(this)
+				.addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+				.build();
+
+		// [START initialize_auth]
+//		mAuth = FirebaseAuth.getInstance();
+		// [END initialize_auth]
+		Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+		startActivityForResult(signInIntent, GOOGLE_SIGN_IN_REQUEST_CODE);
+	}
+
+	// [START auth_with_google]
+//	private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+//		Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+//
+//		AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+//		mAuth.signInWithCredential(credential)
+//				.addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//					@Override
+//					public void onComplete(@NonNull Task<AuthResult> task) {
+//						if (task.isSuccessful()) {
+//							// Sign in success, update UI with the signed-in user's information
+//							Log.d(TAG, "signInWithCredential:success");
+//							FirebaseUser user = mAuth.getCurrentUser();
+//
+//						} else {
+//							// If sign in fails, display a message to the user.
+//							Log.w(TAG, "signInWithCredential:failure", task.getException());
+//							Toast.makeText(PictoryTaleUnityPlayerActivity.this, "Authentication failed.",
+//									Toast.LENGTH_SHORT).show();
+//
+//						}
+//
+//						// [START_EXCLUDE]
+//						//hideProgressDialog();
+//						// [END_EXCLUDE]
+//					}
+//				});
+//	}
+	// [END auth_with_google]
 
 	public void setSystemUiVisibility(boolean statusBarVisible, boolean navBarVisible)
 	{
@@ -273,5 +363,13 @@ public class PictoryTaleUnityPlayerActivity extends MessagingUnityPlayerActivity
 				//Log.e("setStatusBarThemeDark", "setStatusBarThemeDark");
 			}
 		});
+	}
+
+	@Override
+	public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+		// An unresolvable error has occurred and Google APIs (including Sign-In) will not
+		// be available.
+		Log.d(TAG, "onConnectionFailed:" + connectionResult);
+		Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
 	}
 }
